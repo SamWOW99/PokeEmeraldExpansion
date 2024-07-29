4c64316e9fd22105cc3a6f54f3fef587e0c648f8
SamWOW99 <samlaurie@ymail.com>
2024-07-29 00:37:44 -0600

    Changed starters to test updates to type and BST

--- src/starter_choose.c
+++ src/starter_choose.c
@@ -113,8 +113,8 @@ static const u8 sStarterLabelCoords[STARTER_MON_COUNT][2] =
 static const u16 sStarterMon[STARTER_MON_COUNT] =
 {
     SPECIES_ARBOK,
-    SPECIES_TORCHIC,
-    SPECIES_MUDKIP,
+    SPECIES_KINGLER_GIGANTAMAX,
+    SPECIES_PARASECT,
 };
 
 static const struct BgTemplate sBgTemplates[3] =
